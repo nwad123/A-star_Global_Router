7fefed9b08be7d3a51d8ce47ad3598d68f3e0e4f
Nick Waddoups <nick.waddoups@usu.edu>
2024-11-18 13:05:36 -0700

    Finished part C

--- src/MazeRouter.cpp
+++ src/MazeRouter.cpp
@@ -15,48 +15,45 @@
 // 2. Control if any overflow on the path is allowed or not
 ///////////////////////////////////////////////////////////////////////////////
 CostType SimpleGR::routeMaze(Net &net,
-    bool allowOverflow,
-    const Point &botleft,
-    const Point &topright,
-    const EdgeCost &edgeCost,
+    bool allow_overflow,
+    const Point &bot_left,
+    const Point &top_right,
+    const EdgeCost &edge_cost,
     std::vector<Edge *> &path)
 {
-    // find out the ID of the source and sink gcells
-    const IdType sourceGCellId = getGCellId(net.gCellOne);
-    const IdType destGCellId = getGCellId(net.gCellTwo);
-    const auto &snkGCell = getGCell(destGCellId);
+    // Get the ID of the source and destination cells
+    const IdType source_cell_id = getGCellId(net.gCellOne);
+    const IdType dest_cell_id = getGCellId(net.gCellTwo);
 
+    // Get a reference to the source cell (in order to get more information
+    // about the source cell)
+    const auto &source_cell = getGCell(dest_cell_id);
 
-#ifndef NDEBUG
-    std::cout << "Calling routeMaze()\n"
-              << "Bounding box:\n"
-              << "\tBotLeft:  [" << botleft.x << ", " << botleft.y << ", " << botleft.z << "]\n"
-              << "\tTopRight: [" << topright.x << ", " << topright.y << ", " << topright.z << "]\n"
-              << "Source: " << sourceGCellId << "\n"
-              << "Dest:   " << destGCellId << "\n"
-              << " ----- \n\n";
-#endif
+    // the priority queue keeps track of which cells are visited
+    // insert the source cell to the priority queue to indicate that it has been visited
+    priorityQueue.setGCellCost(source_cell_id, 0., 0., NULLID);
 
-    // insert the source gcell into the priority queue
-    priorityQueue.setGCellCost(sourceGCellId, 0., 0., NULLID);
-
-    // Instantiate the Cost function to calculate the Manhattan distance between
-    // two gcells. This distance is used as the heuristic cost for A* search.
+    //@brief calculates the manhattan distance between two cells
+    //  The `ManhattanCost` function object is defined in SimpleGR.h
     ManhattanCost &manhattanDistance = ManhattanCost::getFunc();
 
     //@brief checks if a cell is in the bounding box
     //  Note: this function ignores the z-boundary, as when `routeMaze` is called
-    //  from SimpleGR `0` is always passed for the bot left and top right coord.
+    //  from SimpleGR `0` is always passed for the bot left and top right z-coords.
     //
-    //@param cell Checks if `cell` is in the bounding box
+    //@param cell The location of the cell to check
     //@return Whether or not `cell` is in the bounding box
-    auto in_bounding_box = [&botleft, &topright](const Point &cell) {
-        return (botleft.x <= cell.x && cell.x <= topright.x) && (botleft.y <= cell.y && cell.y <= topright.y);
+    auto in_bounding_box = [&bot_left, &top_right](const Point &cell) {
+        return (bot_left.x <= cell.x && cell.x <= top_right.x) && (bot_left.y <= cell.y && cell.y <= top_right.y);
     };
 
     //@brief given a GCell and an edgeId returns the connecting GCell
-    //  Note: this function has zero error handling, so call it with
-    //  valid args or else :)
+    //
+    // For example, if GCell 1 is connected to GCell 2 via edge 10, this function
+    // call would look like this:
+    //
+    // auto connected_cell = get_connecting_cell(GCell_1, Edge_10);
+    // assert(getGCellID(connected_cell) == 2);
     auto get_connecting_cell = [this](const GCell &from, const IdType across_edge_id) -> GCell & {
         const auto &edge = grEdgeArr[across_edge_id];
         const auto *from_ptr = &from;
@@ -67,13 +64,14 @@ CostType SimpleGR::routeMaze(Net &net,
         }
     };
 
-    //@brief checks if we have reached the destination cell, this function is only
-    //  to provide readability, and it's expected that it will be inlined
-    //  at compile time
-    auto reached_destination = [&destGCellId](const IdType &cellId) { return destGCellId == cellId; };
+    //@brief checks if we have reached the destination cell
+    //
+    //  this function is only written to provide readability, and it's expected
+    //  that it will be inlined at compile time
+    auto reached_destination = [&dest_cell_id](const IdType &cellId) { return dest_cell_id == cellId; };
 
-    //@brief gets a pointer to the edge between two cells
-    //  Note: As usual, there is no error handling :)
+    //@brief Given two cells that are adjacent to one other, return a reference to
+    //  the edge between the cells.
     auto get_edge = [this](const IdType cell1_id, const IdType cell2_id) -> Edge & {
         const auto &cell1 = getGCell(cell1_id);
 
@@ -82,7 +80,7 @@ CostType SimpleGR::routeMaze(Net &net,
 
         IdType edgeId{};
 
-        // check all possible directions the edge could be (as there isn't a better
+        // check all possible directions the edge could be (as I haven't found a better
         // way to get the edge id, which we need to get the edge reference)
         if (cell1_coord.x != cell2_coord.x) {
             if (cell1_coord.x < cell2_coord.x) {
@@ -108,92 +106,74 @@ CostType SimpleGR::routeMaze(Net &net,
         return edge;
     };
 
-    //@brief checks if an edge is viable (checks overflow)
-    auto is_valid_edge = [this, &allowOverflow](const IdType edge_id) -> bool {
+    //@brief Checks if traversing an edge will cause overflow
+    auto causes_overflow = [this, &allow_overflow](const IdType edge_id) -> bool {
+        if (allow_overflow) { return false; }
+
         const auto &edge = grEdgeArr[edge_id];
         const CapType demand = (edge.type == VIA) ? 0 : minWidths[edge.layer] + minSpacings[edge.layer];
 
-        const auto wont_overflow = (edge.usage + demand) <= edge.capacity;
+        const auto will_overflow = (edge.usage + demand) > edge.capacity;
 
-        return wont_overflow or allowOverflow;
+        return will_overflow;
     };
 
-    // A* search kernel
-    // Loop until all "frontiers" in the priority queue are exhausted, or when
-    // the sink gcell is found.
+    // A* search algorithm
+    //
+    // The *best* cell in the priority queue is determined by the cell's "total cost", which
+    // is calculated by summing the distance from source cell to the cell with the the manhattan
+    // distance from the cell to the destination cell.
+    //
+    // In a tradiional A* search you would only insert the single next best location from the
+    // current location, but this implmentation inserts all the next possible locations from
+    // current location
     do {
         const auto this_cell_id = priorityQueue.getBestGCell();
         const auto this_cell_data = priorityQueue.getGCellData(this_cell_id);
 
         priorityQueue.rmBestGCell();
 
-#ifndef NDEBUG
-        std::cout << "\tCurrent best cell: " << this_cell_id << "\n";
-#endif
-
-        // if the current cell is the dest cell we can pop right out of this
-        // loop
+        // if the current cell is the dest cell we can pop out of this loop
         if (reached_destination(this_cell_id)) { break; }
 
-        // get all the edges from the current cell
+        // get all the edges from the current cell in order to look at the
+        // next possible cells
         auto edges = getGCellEdges(this_cell_id);
 
         for (const auto &edgeId : edges) {
-            // if traversing this edge would cause overflow and `allowOverflow = false`
-            // then skip this edge
-            if (!is_valid_edge(edgeId)) {
-#ifndef NDEBUG
-                std::cout << "\tSkipping edge " << edgeId << " because traversing it would cause overflow\n";
-#endif
-                continue;
-            }
 
-            // find the adjacent cell
+            if (causes_overflow(edgeId)) { continue; }
+
+            // get references to the cell connected to the current cell by the current edge
             const auto &this_cell = getGCell(this_cell_id);
             const GCell &connecting_cell = get_connecting_cell(this_cell, edgeId);
             const auto connecting_cell_id = getGCellId(connecting_cell);
 
             // if the cell is out of the bounding box we can skip this loop
-            if (!in_bounding_box(connecting_cell)) {
-#ifndef NDEBUG
-                std::cout << "\tSkipping cell " << connecting_cell_id << " because it has left the bounding box.\n"
-                          << "\t  Cell " << connecting_cell_id << " coords: [" << connecting_cell.x << ", "
-                          << connecting_cell.y << ", " << connecting_cell.z << "]\n";
-#endif
-                continue;
-            }
+            if (!in_bounding_box(connecting_cell)) { continue; }
 
             // calculate the two types of cost
             // manh_cost : heuristic cost between the connecting cell and the destination
             // edge_cost : the cost from the source cell to the connecting cell
-            const auto manh_cost = manhattanDistance(connecting_cell, snkGCell);
-            const auto path_cost = edgeCost(edgeId) + this_cell_data.pathCost;
+            const auto manh_cost = manhattanDistance(connecting_cell, source_cell);
+            const auto path_cost = edge_cost(edgeId) + this_cell_data.pathCost;
 
             // Calculate the total cost as detailed in the PQueue.setGCellCost function
             const auto total_cost = manh_cost + path_cost;
-
-#ifndef NDEBUG
-            std::cout << "\tCost to: " << connecting_cell_id << " = " << manh_cost << " + " << path_cost << " = "
-                      << manh_cost + path_cost << "\n";
-#endif
 
             // insert the neighbor cell into the priority queue
             priorityQueue.setGCellCost(connecting_cell_id, manh_cost, total_cost, this_cell_id);
         }
 
-#ifndef NDEBUG
-        std::cout << "\n";
-#endif
-
     } while (!priorityQueue.isEmpty());
 
     // now backtrace and build up the path, if we found one
     // back-track from sink to source, and fill up 'path' vector with all the edges that are traversed
-    if (priorityQueue.isGCellVsted(destGCellId)) {
+    if (priorityQueue.isGCellVsted(dest_cell_id)) {
         // YOUR backtrace CODE GOES IN HERE
-        auto current_id = destGCellId;
+        auto current_id = dest_cell_id;
 
-        while (current_id != sourceGCellId) {
+        while (current_id != source_cell_id) {
             const auto &current_node = priorityQueue.getGCellData(current_id);
             const auto parent_id = current_node.parentGCell;
 
@@ -205,8 +185,8 @@ CostType SimpleGR::routeMaze(Net &net,
     }
 
     // calculate the accumulated cost of the path
-    const CostType finalCost = priorityQueue.isGCellVsted(destGCellId)
-                                   ? priorityQueue.getGCellData(destGCellId).pathCost
+    const CostType finalCost = priorityQueue.isGCellVsted(dest_cell_id)
+                                   ? priorityQueue.getGCellData(dest_cell_id).pathCost
                                    : std::numeric_limits<CostType>::max();
 
     // clean up
